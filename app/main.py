b2d61f8a31207780d56e9b02fe6e8fab6da5919b
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-09-08 20:11:59 +0200

    feat: init move_file function

--- app/main.py
+++ app/main.py
@@ -1,1 +1,27 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3:
+        return "> 3"
+
+    if parts[0] != "mv":
+        return "not mv"
+
+    file_name = parts[1]
+    dir_file_name = parts[2]
+
+    directory = os.path.dirname(dir_file_name)
+
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+
+    with open(file_name, "r") as file_in:
+        old_content = file_in.read()
+
+    with open(dir_file_name, "w") as file_out:
+        file_out.write(old_content)
+
+    return os.remove(file_name)
